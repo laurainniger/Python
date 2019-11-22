b5386948505d64a3fa132213a8afe3ccb0beacc4
laurainniger <laurainniger@hotmail.com>
2019-11-22 10:04:22 -0500

    Add files via upload

---
+++ places.py
@@ -0,0 +1,10 @@
+
+
+place = ["Pakistan", "England", "Iran", "USA"]
+for country in place:
+  if country == "England":
+    print(country + "... Bobbies on bicycles! Yes, I lived here.")
+  elif country == "USA":
+    print(country + " is where I live now.")
+  else:
+    print(country + " is a place I used to live.")
