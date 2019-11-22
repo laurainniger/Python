aba49bd63ccef8d608c57a3f16fb41d8967dbef5
laurainniger <laurainniger@hotmail.com>
2019-11-22 10:00:19 -0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,15 @@
+#display integers and floats
+x = 51
+y = 50.5
+if x < y:
+  print(float(x), " is less than ", float(y))
+elif x == y:
+  print(float(x), " is equal to ", float(y))
+else: 
+  print(float(x), "is greater than", float(y))
+
+# display complex numbers
+
+print("Define j to solve the complex numbers:")
+print(complex(x))
+print(complex(y))
