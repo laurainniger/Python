03dbb1911882d47a2c1ccd174185bca355ab4b1b
laurainniger <laurainniger@hotmail.com>
2019-12-06 22:33:56 -0500

    Add files via upload

--- travel.py
+++ travel.py
@@ -1,4 +1,7 @@
 lines = open("projects3/travelers.csv").read().split("\n")
 print(lines)
 for line in lines:
-    print(line)
+    print(line.split())
+for line in lines:
+    if "Jeff" in line:
+        print("Jeff: " + line)
