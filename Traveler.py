8a347a91a203d33e45a374ea79df2404f744870a
laurainniger <laurainniger@hotmail.com>
2019-12-07 16:03:43 -0500

    Add files via upload

---
+++ Traveler.py
@@ -0,0 +1,7 @@
+class Traveler:
+
+    def __init__(self, name, place, gender, title):
+        self.name = name
+        self.place = place
+        self.gender = gender
+        self.title = title
---
+++ Travelers.py
@@ -0,0 +1,7 @@
+from Traveler import Traveler
+
+traveler1 = Traveler("Susie", "China", "female", "marketing associate")
+traveler2 = Traveler("Jeff", "Hong Kong", "male", "tool and die maker")
+
+print(traveler1.name + " in " + traveler1.place + " is a " + traveler1.title + ".")
+print(traveler2.name + " in " + traveler2.place + " is a " + traveler2.title + ".")
